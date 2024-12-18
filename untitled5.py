efbadaac9fb3b7f86b6b2fe3fb33615e104d0fe7
xurshidbekodilov <167975176+xurshidbekodilov@users.noreply.github.com>
2024-12-18 12:03:19 +0500

    uploaded

---
+++ untitled5.py
@@ -0,0 +1,41 @@
+pip install opencv-python
+
+from google.colab.patches import cv2_imshow
+import cv2
+rasm=cv2.imread("candyStick.png")
+cv2_imshow(rasm)
+
+oqqora=cv2.cvtColor(rasm,cv2.COLOR_BGR2GRAY)
+cv2_imshow(oqqora)
+
+from google.colab.patches import cv2_imshow
+import cv2
+rasm=cv2.imread("celebrateImg.png")
+cv2_imshow(rasm)
+
+oqqora=cv2.cvtColor(rasm,cv2.COLOR_BGR2GRAY)
+cv2_imshow(oqqora)
+
+from google.colab.patches import cv2_imshow
+import cv2
+rasm=cv2.imread("celebrateMobileImg.png")
+cv2_imshow(rasm)
+
+oqqora=cv2.cvtColor(rasm,cv2.COLOR_BGR2GRAY)
+cv2_imshow(oqqora)
+
+from google.colab.patches import cv2_imshow
+import cv2
+rasm=cv2.imread("christmasTree.png")
+cv2_imshow(rasm)
+
+oqqora=cv2.cvtColor(rasm,cv2.COLOR_BGR2GRAY)
+cv2_imshow(oqqora)
+
+from google.colab.patches import cv2_imshow
+import cv2
+rasm=cv2.imread("christmasWreath.png")
+cv2_imshow(rasm)
+
+oqqora=cv2.cvtColor(rasm,cv2.COLOR_BGR2GRAY)
+cv2_imshow(oqqora)
